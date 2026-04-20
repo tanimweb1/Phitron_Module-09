c01d8c1ac4dd790a7a32b20fa2249f7055f40638
Shahriar Mahmud Tanim <157669438+tanimweb1@users.noreply.github.com>
2026-04-20 22:57:59 +0600

    Update practice.c

--- practice.c
+++ practice.c
@@ -115,3 +115,21 @@ return 0;
 }
 
 
+
+
+
+
+
+#include<stdio.h>
+int main(){
+
+    //swaping values
+int a,b,temp;
+scanf("%d%d",&a,&b);
+
+temp = a;
+a = b;
+b = temp;
+printf("a is %d & b is %d",a,b);
+return 0;
+}

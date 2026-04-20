dd8549622ba824d2fd62afe7b0ba3c467719e3d7
Shahriar Mahmud Tanim <157669438+tanimweb1@users.noreply.github.com>
2026-04-20 21:52:32 +0600

    Implement array element deletion
    Added functionality to delete an element from an array.

--- practice.c
+++ practice.c
@@ -28,3 +28,36 @@ int a[n+1];
 
 return 0;
 }
+
+
+
+
+
+
+
+#include<stdio.h>
+int main(){
+
+    //delete an element from a array
+
+int n;
+scanf("%d",&n);
+
+int a[n];
+for(int i=0;i<n;i++){
+    scanf("%d",&a[i]);
+}
+int idx;
+scanf("%d",&idx);
+
+for(int i = idx;i<n-1;i++){
+    a[i] = a[i+1];
+}
+
+for(int i = 0;i<n-1;i++){
+    printf("%d ",a[i]);
+}
+
+
+return 0;
+}

9a669ed3be5aadbd25dc292070b6624382493982
Shahriar Mahmud Tanim <157669438+tanimweb1@users.noreply.github.com>
2026-04-20 22:02:52 +0600

    Update practice.c

--- practice.c
+++ practice.c
@@ -61,3 +61,38 @@ for(int i = 0;i<n-1;i++){
 
 return 0;
 }
+
+
+
+
+
+
+
+#include<stdio.h>
+int main(){
+
+    //delete an element from a array
+
+int n;
+scanf("%d",&n);
+
+int a[n];
+for(int i=0;i<n;i++){
+    scanf("%d",&a[i]);
+}
+int idx;
+scanf("%d",&idx);
+
+for(int i = idx;i<n-1;i++){
+    a[i] = a[i+1];
+}
+n--;
+for(int i = 0;i<n;i++){
+    printf("%d ",a[i]);
+}
+
+
+return 0;
+}
+
+

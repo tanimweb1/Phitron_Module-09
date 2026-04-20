5d26f19ca1779629daa341350ae69f88cd70b349
Shahriar Mahmud Tanim <157669438+tanimweb1@users.noreply.github.com>
2026-04-20 20:40:21 +0600

    Add files via upload

---
+++ practice.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+
+    //insert a value in a array
+
+    int n;
+    scanf("%d",&n);
+int a[n];
+    for(int i = 0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+
+    int idx,val;
+    scanf("%d%d",&idx,&val);
+
+    for(int i = n+1; i>idx;i--)
+
+
+
+return 0;
+}
